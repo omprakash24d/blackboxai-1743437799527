198ef5a7d9c153c4b1d4dfbb9a032d5f1ef82dd3
Om Prakash <omprakash24d@gmail.com>
2025-03-31 21:46:59 +0530

    Add src/navigation/AppNavigator.js 1743437817589

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,31 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useAuth } from "@clerk/clerk-expo";
+import AuthScreen from "../screens/AuthScreen";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  const { isSignedIn } = useAuth();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {!isSignedIn ? (
+          <Stack.Screen 
+            name="Auth" 
+            component={AuthScreen} 
+            options={{ headerShown: false }}
+          />
+        ) : (
+          <Stack.Screen 
+            name="Home" 
+            component={HomeScreen}
+            options={{ title: 'Welcome' }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
